8f8f99866c888599b938b587136c9d7410ffbafc
Raúl M <between13131@gmail.com>
2022-03-11 00:00:10 +0100

    Remove unused import

--- src/main/java/com/oskarsmc/send/command/SendCommand.java
+++ src/main/java/com/oskarsmc/send/command/SendCommand.java
@@ -25,7 +25,6 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicInteger;
 
